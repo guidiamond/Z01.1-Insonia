c3fc9202ebc55f5845710f7942cd8ed32a5474f4
roguetaver <andretavernaro@terra.com.br>
2019-03-07 16:05:32 -0300

    Dmux2way corrigido

--- Projetos/C-LogicaCombinacional/src/rtl/DMux2Way.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/DMux2Way.vhd
@@ -1,22 +1,18 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity Nor8Way is
+entity DMux2Way is
 	port ( 
 			a:   in  STD_LOGIC;
-			b:   in  STD_LOGIC;
-			c:   in  STD_LOGIC;
-			d:   in  STD_LOGIC;
-			e:   in  STD_LOGIC;
-			f:   in  STD_LOGIC;
-			g:   in  STD_LOGIC;
-			h:   in  STD_LOGIC;
-			q:   out STD_LOGIC);
+			sel: in  STD_LOGIC;
+			q0:  out STD_LOGIC;
+			q1:  out STD_LOGIC);
 end entity;
 
-architecture arch of Nor8Way is
+architecture rtl of DMux2Way is
 begin
 
-q <= '1' when a='0' and b='0' and c='0' and d='0' and e='0' and f='0' and g='0' and h='0' else '0';
+q0 <= a when sel='0' else '0';
+q1 <= a when sel='1' else '0';
 
 end architecture;
