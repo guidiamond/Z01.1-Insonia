14c8bb81f38d6d2c83b328a6c40cd47515e6282e
guidiamond <guilherme.batista770@gmail.com>
2019-03-12 18:23:27 -0300

    correcao MUX16

--- Projetos/C-LogicaCombinacional/src/rtl/Mux16.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/Mux16.vhd
@@ -11,7 +11,7 @@ end entity;
 
 architecture arch of Mux16 is
 begin
-
-q <= (a and not sel) or (b and sel); 
+	q <= a when (sel = '0') else b;
+	--q <= (a and not sel) or (b and sel); 
 
 end architecture;
