9aee99aba8d360b786aa4f680ac7295dbc75c890
roguetaver <andretavernaro@terra.com.br>
2019-03-22 09:25:01 -0300

    binary nao finalizado

--- Projetos/E-LogicaSequencial/src/rtl/BinaryDigit.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/BinaryDigit.vhd
@@ -5,21 +5,38 @@
 Library ieee;
 use ieee.std_logic_1164.all;
 
+
 entity BinaryDigit is
 	port(
 		clock:   in STD_LOGIC;
-		input:   in STD_LOGIC;
+		inp:   in STD_LOGIC;
 		load:    in STD_LOGIC;
-		output: out STD_LOGIC
+		outp: out STD_LOGIC
 	);
 end entity;
 
 architecture rtl of BinaryDigit is
   -- Aqui declaramos sinais (fios auxiliares)
   -- e componentes (outros módulos) que serao
-  -- utilizados nesse modulo.
+  -- utilizados nesse modulo.	
+  component Mux2Way is
+    port(
+      a,b,sel:      in STD_LOGIC;   -- entradas
+      q: out STD_LOGIC   --saida
+    );
+  end component;
+
+  component  FlipFlopD is
+    port(
+      clock,d,clear,preset:   in STD_LOGIC;   -- entradas
+      q: out STD_LOGIC   --saida
+    );
+  end component;
+SIGNAL muxsaida : STD_LOGIC;
 
 begin
+	f1: Mux2Way port map (outp ,inp ,load ,muxsaida );
+	f2: FlipFlopD port map (clock ,muxsaida ,'0' ,'0' ,outp );
 
 
 end architecture;
