78dbca0bdb21d38b0360ea1d9490dbd301f7f9e6
4rfel <rafaels6@al.insper.edu.br>
2019-04-22 19:16:18 -0300

    add novo mux AM/D

--- Projetos/G-Computador/src/rtl/ControlUnit.vhd
+++ Projetos/G-Computador/src/rtl/ControlUnit.vhd
@@ -15,7 +15,7 @@ entity ControlUnit is
 		muxALUI_A                   : out STD_LOGIC;                     -- mux que seleciona entre instrução e ALU para reg. A
 		muxAM_ALU                   : out STD_LOGIC;                     -- mux que seleciona entre reg. A e Mem. RAM para ALU
 		muxSD_ALU                   : out STD_LOGIC;                     -- mux que seleciona entre reg. S e reg. D
-		muxA_M                      : out STD_LOGIC;					 -- mux que seleciona entre reg. A e reg. Ms
+		muxAM_D                      : out STD_LOGIC;					 -- mux que seleciona entre reg. A e reg. M
 		zx, nx, zy, ny, f, no       : out STD_LOGIC;                     -- sinais de controle da ALU
 		loadA, loadD, loadS, loadM, loadPC : out STD_LOGIC               -- sinais de load do reg. A, reg. D,
                                                                      -- Mem. RAM e Program Counter
