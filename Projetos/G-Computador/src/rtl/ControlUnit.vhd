fa236edda097f64e3a6d315211ca37f84ec7181e
Rafael Corsi <rafael.corsi@insper.edu.br>
2019-04-24 15:29:04 -0300

    Update ControlUnit.vhd

--- Projetos/G-Computador/src/rtl/ControlUnit.vhd
+++ Projetos/G-Computador/src/rtl/ControlUnit.vhd
@@ -15,8 +15,9 @@ entity ControlUnit is
 		muxALUI_A                   : out STD_LOGIC;                     -- mux que seleciona entre instrução e ALU para reg. A
 		muxAM_ALU                   : out STD_LOGIC;                     -- mux que seleciona entre reg. A e Mem. RAM para ALU
 		muxSD_ALU                   : out STD_LOGIC;                     -- mux que seleciona entre reg. S e reg. D
+	    	muxAMD_ALU                  : out STD_LOGIC;                     -- mux que seleciona entre reg. A e Mem. RAM para ALU
 		zx, nx, zy, ny, f, no       : out STD_LOGIC;                     -- sinais de controle da ALU
-		loadA, loadD, loadS, loadM, loadPC : out STD_LOGIC               -- sinais de load do reg. A, reg. D,
+	   	loadA, loadD, loadS, loadM, loadPC : out STD_LOGIC               -- sinais de load do reg. A, reg. D,
                                                                      -- Mem. RAM e Program Counter
     );
 end entity;
