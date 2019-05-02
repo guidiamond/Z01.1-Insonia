c96e80e46781d410c77ae1d140c7f1770efa6146
roguetaver <andretavernaro@terra.com.br>
2019-05-02 17:59:39 -0300

    cpu tentativa

--- Projetos/G-Computador/src/rtl/CPU.vhd
+++ Projetos/G-Computador/src/rtl/CPU.vhd
@@ -108,5 +108,26 @@ architecture arch of CPU is
 
 
 begin
+   muxALUI: Mux16 port map(s_ALUout,instruction,c_muxALUI_A,s_muxALUI_Aout);
+   muxSD: Mux16 port map(s_regSout,s_regDout,c_muxSD_ALU,s_muxSDout);
+   muxAMD: Mux16 port map(s_regDout,s_muxAM_out,c_muxAMD_ALU,s_muxAMD_ALUout);
+   muxAM: Mux16 port map(s_regAout,inM,c_muxAM,s_muxAM_out);
+   RgA: Register16 port map (clock,s_muxALUI_Aout,c_loadA,s_regAout);
+   RgD: Register16 port map(clock,s_ALUout,c_loadD,s_regDout);
+   RgS: Register16 port map(clock,s_ALUout,c_loadS,s_regSout);
+   PC_port: PC port map(clock,'1',c_loadPC,reset,s_regAout,s_pcout);
+   ALU_port: ALU port map(s_muxSDout,s_muxAMD_ALUout,c_zx,c_nx,c_zy,c_ny,
+                          c_f,c_no,c_zr,c_ng,s_ALUout);
+   CU: ControlUnit port map (instruction,c_zr,c_ng,c_muxALUI_A,
+                             c_muxAM,c_muxAMD_ALU,c_muxSD_ALU,c_zx,
+                             c_nx,c_zy,c_ny,c_f,c_no,
+                             c_loadA,c_loadD,c_loadS,
+                             writeM,c_loadPC);
+   
+   outM <= s_ALUout;
+   addressM <= s_regAout(14 downto 0);
+   pcout <= s_pcout(14 downto 0);
+
+
 
 end architecture;
