46bcd24fb4c3cb0c79c9fc7480f5288b3b1080b3
Rafael Corsi <rafael.corsi@insper.edu.br>
2019-05-15 19:08:06 -0300

    Update AssembleTest.java

--- Projetos/H-Assembler/Assembler/src/test/java/assembler/AssembleTest.java
+++ Projetos/H-Assembler/Assembler/src/test/java/assembler/AssembleTest.java
@@ -26,7 +26,7 @@ public class AssembleTest {
     }
 
     @Test
-    public void fillSymbolTable() throws IOException, InvalidAssemblyException {
+    public void fillSymbolTable() throws IOException {
         // Cria tabela de símbolos
         SymbolTable table = assembler.fillSymbolTable();
         assertTrue("R1",table.contains("R1")==true);
